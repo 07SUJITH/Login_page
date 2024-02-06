0844780d25c4b32be6e921a981ed9dd37fd74223
joelsibyvarghese <joelpadiyil@gmail.com>
2024-02-06 12:08:04 +0530

    removed useCallback

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
-  const handleSubmit = useCallback(async (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const userCredentials = { username, password };
 
@@ -22,14 +22,14 @@ const Login = () => {
       }
       setErrorMessage(errorMsg);
     }
-  }, [username, password]);
+  }
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Sign in to your account
+            Sign
           </h2>
         </div>
         <form className="mt-8 space-y-6 " onSubmit={handleSubmit}>
